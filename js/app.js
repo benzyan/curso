40e2d1bc2abe4fec2a316e16d28a06aea7b18200
benzyan <73322521+benzyan@users.noreply.github.com>
2020-12-17 05:56:14 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+//el script del contenido del curso//
+
+function myFunction() {
+  var x = document.getElementById("myDiv1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionn() {
+  var x = document.getElementById("myDiv2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function MyFunction() {
+  var x = document.getElementById("myDiv3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+  
+
+ 
+
+ 
